5d7370980338328d919109550313f7f8f585e182
Aditya Prasad Panigrahi <adityappanigrahi@gmail.com>
2026-07-24 15:04:17 +0530

    lab

---
+++ app/lab/page.tsx
@@ -0,0 +1,10 @@
+import { BlogNav } from "@/components/blog/blog-nav";
+
+export default function LabPage() {
+  return (
+    <>
+      <BlogNav />
+      <main className="min-h-screen bg-white" />
+    </>
+  );
+}
